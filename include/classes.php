4cb8bfce25a680ad50bf2fcb6130ba2a19a93dc7
frostkom <martin.fors@frostkom.se>
2017-09-11 14:37:49 +0200

    Clean up widget

--- include/classes.php
+++ include/classes.php
@@ -11,12 +11,16 @@ class widget_theme_logo extends WP_Widget
 
 		$control_ops = array('id_base' => 'theme-logo-widget');
 
+		$this->arr_default = array();
+
 		parent::__construct('theme-logo-widget', __("Logo", 'lang_theme'), $widget_ops, $control_ops);
 	}
 
 	function widget($args, $instance)
 	{
 		extract($args);
+
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo $before_widget
 			.get_logo_theme()
@@ -27,6 +31,8 @@ class widget_theme_logo extends WP_Widget
 	{
 		$instance = $old_instance;
 
+		$new_instance = wp_parse_args((array)$new_instance, $this->arr_default);
+
 		//$instance[''] = strip_tags($new_instance['']);
 
 		return $instance;
@@ -34,10 +40,7 @@ class widget_theme_logo extends WP_Widget
 
 	function form($instance)
 	{
-		$defaults = array(
-			//'' => "",
-		);
-		$instance = wp_parse_args((array)$instance, $defaults);
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo "<p>".__("No need for settings here, we'll take care of the rest", 'lang_theme')."</p>";
 	}
@@ -52,14 +55,18 @@ class widget_theme_menu extends WP_Widget
 			'description' => __("Display menu", 'lang_theme')
 		);
 
-		$control_ops = array('id_base' => 'theme-menu-widget');
+		$this->arr_default = array(
+			'theme_menu_type' => "",
+		);
 
-		parent::__construct('theme-menu-widget', __("Menu", 'lang_theme'), $widget_ops, $control_ops);
+		parent::__construct('theme-menu-widget', __("Menu", 'lang_theme'), $widget_ops);
 	}
 
 	function widget($args, $instance)
 	{
 		extract($args);
+
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo $before_widget
 			.get_menu_theme(array('type' => $instance['theme_menu_type'], 'where' => $id))
@@ -70,6 +77,8 @@ class widget_theme_menu extends WP_Widget
 	{
 		$instance = $old_instance;
 
+		$new_instance = wp_parse_args((array)$new_instance, $this->arr_default);
+
 		$instance['theme_menu_type'] = strip_tags($new_instance['theme_menu_type']);
 
 		return $instance;
@@ -77,10 +86,7 @@ class widget_theme_menu extends WP_Widget
 
 	function form($instance)
 	{
-		$defaults = array(
-			'theme_menu_type' => "",
-		);
-		$instance = wp_parse_args((array)$instance, $defaults);
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo "<div class='mf_form'>"
 			.show_select(array('data' => get_menu_type_for_select(), 'name' => $this->get_field_name('theme_menu_type'), 'text' => __("Menu Type", 'lang_theme'), 'value' => $instance['theme_menu_type']))
