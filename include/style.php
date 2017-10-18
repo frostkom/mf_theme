9a688b0cbc2f96689c221cbb7139faf397217717
frostkom <martin.fors@frostkom.se>
2017-10-18 12:40:49 +0200

    Remove init_style

--- include/style.php
+++ include/style.php
@@ -9,8 +9,6 @@ if(!defined('ABSPATH'))
 	require_once($folder."wp-load.php");
 }
 
-//do_action('init_style');
-
 $options_fonts = get_theme_fonts();
 
 list($options_params, $options) = get_params();
