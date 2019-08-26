085a8baef256345cf937609d014b6cbb210f05e3
frostkom <martin.fors@frostkom.se>
2019-08-26 13:56:36 +0200

    Use Theme Core field to include external resources

--- include/classes.php
+++ include/classes.php
@@ -300,6 +300,9 @@ class mf_theme
 		$theme_version = get_theme_version();
 
 		mf_enqueue_style('style', $template_url."/include/style.php", $theme_version);
+
+		$obj_theme_core->get_external_css($theme_version);
+
 		mf_enqueue_script('script_theme', $template_url."/include/script.js", array(
 			'template_url' => $template_url,
 			'hamburger_collapse_if_no_space' => (isset($obj_theme_core->options['hamburger_collapse_if_no_space']) && $obj_theme_core->options['hamburger_collapse_if_no_space'] == 2),
